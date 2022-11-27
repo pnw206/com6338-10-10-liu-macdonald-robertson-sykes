7ca4eecdd9ed0b516005e2bad70f6f5dd2bc0d84
pnw206 <robertson.carmen@ufl.edu>
2022-11-26 18:36:25 -0800

    update - indv.player page
    @mnsykes @catsociety so I was able to get all this done. I had to use two different URLs in order to fetch the data. In the console log it appears as though it is accurately calling data but I cant seem to get it to identify an indv player or pull the data onto the page. Any ideas?
    
    I added the search bar since the nav seemed better than having to go back to the team page. definitely needs some touch ups but Im hitting a wall with my limited JS skills.

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,16 +1,117 @@
-
+let form = document.querySelector("form");
 const indvPlayerTblBody = document.getElementById("indv-player-table");
 
-fetch("https://api-nba-v1.p.rapidapi.com/players?id=265", {
-	"method": "GET",
-	"headers": {
-        "X-RapidAPI-Key": "c204799958msh8bd639d820cabb0p1137f0jsn255151fddc1c",
-		"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
-	}
-})
-.then((response) => response.json() {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
+form.onsubmit = function (e) {
+	e.preventDefault()
+	var searchTerm = this.indvPlayerName.value
+	if (!searchTerm) return
+	form.indvPlayerName.value = " "
+	fetch("https://api-nba-v1.p.rapidapi.com/players?id=265", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+			"x-rapidapi-key": "XxXxXxXxXxXxXxXxXxXxXxXx"
+		}
+	})
+		.then(response => {
+			console.log(response);
+		})
+		.then((players) => {
+			//console.log(players.response)
+			players.response.map((players, key) => {
+				let indvPlayerId = player.id;
+				let indvPlayerName = `${players.firstname} ${players.lastname}`;
+				if (player.leagues.standard.active) {
+					let tbleRow = document.createElement("tr");
+					tblRow.innerHTML = `
+			<tr>
+			${players.firstname} ${players.lastname}
+			</tr>
+			<tr> 
+			${players.birth.date}
+			</tr> 
+			<tr> 
+			${players.height}
+			</tr> 
+			<tr> 
+			${players.weight.pounds}
+			</tr> 
+			<tr> 
+			${players.college}
+			</tr> 
+			<tr> 
+			${players.nba.start}
+			</tr> 
+			<tr> 
+			${players.nba.pro}
+			</tr> 
+			`
+					indvPlayerTblBody.appendChild(tbleRow);
+				}
+			})
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
+
+const statsMostCurrentGameTable = document.getElementById("stats-most-current-game")
+
+form.onsubmit = function (e) {
+	e.preventDefault()
+	var searchTerm = this.indvPlayerName.value
+	if (!searchTerm) return
+	form.indvPlayerName.value = " "
+	fetch("https://api-nba-v1.p.rapidapi.com/players/statistics?season=2020&id=734", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+			"x-rapidapi-key": "c204799958msh8bd639d820cabb0p1137f0jsn255151fddc1c"
+		}
+	})
+		.then(response => {
+			console.log(response);
+		})
+		.then((players) => {
+			//console.log(players.response)
+			players.response.map((players, key) => {
+				if (indvPlayerId === player.id) {
+					let tbleRow = document.createElement("tr");
+					tblRow.innerHTML = `
+			<tr>
+			${players.statistics.points} 
+			</tr>
+			<tr> 
+			${players.statistics.pos}
+			</tr> 
+			<tr> 
+			${players.statistics.totReb}
+			</tr> 
+			<tr> 
+			${players.statistics.offReb}
+			</tr> 
+			<tr> 
+			${players.statistics.defReb}
+			</tr> 
+			<tr> 
+			${players.statistics.assists}
+			</tr> 
+			<tr> 
+			${players.statistics.steals}
+			</tr> 
+			<tr> 
+			${players.statistics.turnovers}
+			</tr> 
+			<tr> 
+			${players.statistics.blocks}
+			</tr> 
+			`;
+					statsMostCurrentGameTable.appendChild(tbleRow);
+				}
+			})
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
+
